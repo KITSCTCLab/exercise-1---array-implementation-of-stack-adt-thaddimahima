28664b34df31e17fc9c9e72baef62bf362c90fc9
URK21CS3022MAHIMA <109952027+URK21CS3022MAHIMA@users.noreply.github.com>
2022-07-25 15:10:43 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,23 +3,28 @@ class Stack:
     def __init__(self, size):
         self.items = []
         self.size = size
+        self.top =-1
 
     def is_empty(self):
-        # Write code here
+        return self.top==-1
 
     def is_full(self):
-        # Write code here
+        return self.top == self.n-1
 
     def push(self, data):
         if not self.is_full():
-            # Write code here
+           self.top =+1
+        self.items(self.top) =data
 
     def pop(self):
         if not self.is_empty():
-            # Write code here
+          x=self.item[self.top]
+            self.top-=1
+            return x
 
     def status(self):
-        # Write code here
+        for i in range(self.top+1):
+            print(self.item[i]) 
 
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
